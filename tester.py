6f7f3f71e8927a83ab0b89d814c5ce390ae4766e
Shinyi Chen <jchen@isi.edu>
2019-04-29 06:58:53 -0700

    Runner files

--- tester.py
+++ tester.py
@@ -10,12 +10,9 @@ from Updater import Updater
 from datetime import datetime
 
 if __name__ == '__main__':
-    endpoint = sys.argv[1]  # without repo
-    repo = sys.argv[2]
-    if len(sys.argv) > 3:
-        graph = sys.argv[3]
-    else:
-        graph = None
+    endpoint = 'http://gaiadev01.isi.edu:7200/repositories'
+    repo = 'gaia0404ta1'
+    graph = None
 
     endpoint_url = endpoint + '/' + repo
     outdir = 'store_data/' + repo
@@ -31,21 +28,21 @@ if __name__ == '__main__':
     generate_dataframe(endpoint_url, outdir)
 
     print('Augmenting dataframe with translation columns... ', datetime.now().isoformat())
-    add_trasl_cols(outdir + '/entity_all.h5', outdir)
+    # add_trasl_cols(outdir + '/entity_all.h5', outdir)
 
-    print('Generating entity clusters... ', datetime.now().isoformat())
-    gen_entity_clusters_baseline(outdir + '/entity_trans_all.h5', outdir)
-
-    print('Generating event clusters... ', datetime.now().isoformat())
-    gen_event_clusters(endpoint_url, outdir)
-
-    print('Insert into GraphDB... ', datetime.now().isoformat())
-    up = Updater(endpoint_url, repo, outdir, graph, True)
-    up.run_load_jl()
-    up.run_delete_ori()
-    up.run_system()
-    up.run_entity_nt()
-    up.run_event_nt()
-    up.run_relation_nt()
+    # print('Generating entity clusters... ', datetime.now().isoformat())
+    # gen_entity_clusters_baseline(outdir + '/entity_trans_all.h5', outdir)
+    #
+    # print('Generating event clusters... ', datetime.now().isoformat())
+    # gen_event_clusters(endpoint_url, outdir)
+    #
+    # print('Insert into GraphDB... ', datetime.now().isoformat())
+    # up = Updater(endpoint_url, repo, outdir, graph, True)
+    # up.run_load_jl()
+    # up.run_delete_ori()
+    # up.run_system()
+    # up.run_entity_nt()
+    # up.run_event_nt()
+    # up.run_relation_nt()
     # up.run_insert_proto()
     # up.run_super_edge()

f16a01159b69af483cbb14c4e5c825598b50188c
Shinyi Chen <jchen@isi.edu>
2019-06-24 20:27:55 -0700

    Update namespaces to 2019

--- gen_event_clusters.py
+++ gen_event_clusters.py
@@ -3,11 +3,11 @@ import json
 
 namespaces_str = """
 @prefix : <https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/AidaDomainOntologiesCommon#> .
-@prefix aida: <https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/InterchangeOntology#> .
+@prefix aida: <https://tac.nist.gov/tracks/SM-KBP/2019/ontologies/InterchangeOntology#> .
 @prefix dc: <http://purl.org/dc/elements/1.1/> .
-@prefix domainOntology: <https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/SeedlingOntology> .
-@prefix ldc: <https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/LdcAnnotations#> .
-@prefix ldcOnt: <https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/LDCOntology#> .
+@prefix domainOntology: <https://tac.nist.gov/tracks/SM-KBP/2019/ontologies/SeedlingOntology> .
+@prefix ldc: <https://tac.nist.gov/tracks/SM-KBP/2019/ontologies/LdcAnnotations#> .
+@prefix ldcOnt: <https://tac.nist.gov/tracks/SM-KBP/2019/ontologies/LDCOntology#> .
 @prefix owl: <http://www.w3.org/2002/07/owl#> .
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .

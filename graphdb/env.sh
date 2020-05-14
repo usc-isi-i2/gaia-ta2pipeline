bda44a4fb3804a86a4fbfe86cf38313fc7b59e83
GreatYYX <bigyyx@gmail.com>
2020-05-13 17:52:55 -0700

    Delete env.sh

--- graphdb/env.sh
+++
@@ -1,6 +0,0 @@
-export AUTH=$(cat "admin.auth")
-export ENDPOINT=http://localhost:7200
-export GRAPHDB_BASE=/lfs1/gaia/graphdb-free-8.6.1
-export GRAPHDB_ADMIN_PASSWD=gaia@isi
-export GRAPHDB_EXPORT_UTIL_PATH=./nlp-util/nlp-core-open/target/appassembler/bin/exportGraphDB
-export GRAPHDB_PID_FILE=/tmp/graphdb.pid

1d971570863a05bdde151598234f50bcb87375b8
greatyyx <bigyyx@gmail.com>
2020-09-22 16:40:13 -0700

    fix error log bug

--- pipeline/importer.py
+++ pipeline/importer.py
@@ -59,7 +59,7 @@ class Importer(object):
         self.logger.debug('exec_sh:', s)
         process = subprocess.Popen(s, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        if stderr == b'':
+        if stderr != b'':
             self.logger.error('exec_sh: %s . stderr: %s', s, stderr)
         return stdout, stderr
 

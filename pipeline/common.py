54d932500bffea574dfd4cb4dbbdf0e3a50c7122
greatyyx <bigyyx@gmail.com>
2020-10-30 16:15:30 -0700

    error out if subprocess return code is not 0

--- pipeline/common.py
+++ pipeline/common.py
@@ -5,6 +5,6 @@ def exec_sh(s, logger):
     logger.debug('exec_sh:' + s)
     process = subprocess.Popen(s, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    if stderr != b'':
-        logger.error('exec_sh: %s . stderr: %s', s, stderr)
+    if process.returncode != 0 or stderr != b'':
+        logger.error('exec_sh: %s . return code: %s . stderr: %s', s, process.returncode, stderr)
     return stdout, stderr

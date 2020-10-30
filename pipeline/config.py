35df8a3205c06eebb7a405c0ccb8cb4a546d8b70
greatyyx <bigyyx@gmail.com>
2020-10-30 15:06:48 -0700

    enable setting logging level from env variable

--- pipeline/config.py
+++ pipeline/config.py
@@ -3,6 +3,16 @@ import sys
 import os
 
 
+DEBUG_LEVEL = {
+    # 'CRITICAL': logging.CRITICAL,
+    # 'FATAL': logging.FATAL,
+    'ERROR': logging.ERROR,
+    'WARNING': logging.WARNING,
+    'INFO': logging.INFO,
+    'DEBUG': logging.DEBUG,
+}
+
+
 def get_env_var(name, optional=False, default=None):
     v = os.environ.get(name)
     if not v:
@@ -23,9 +33,9 @@ def get_config():
             'wd_to_fb_file': '../pipeline_test/res/df_wd_fb_20200803.csv',
             'input_dir': '../pipeline_test/input',
             'output_dir': '../pipeline_test/output',
-            'run_name': 'cmu-test',
+            'run_name': 'uiuc-test',
             'temp_dir': '../pipeline_test/temp',
-            'namespace_file': '../pipeline_test/res/aida-namespaces-cmu.tsv',
+            'namespace_file': '../pipeline_test/res/aida-namespaces-uiuc.tsv',
             'logging_level': logging.INFO,
             'num_of_processor': 4,
             'kb_to_fbid_mapping': None, #'../pipeline_test/res/kb_to_wd_mapping.json',
@@ -39,7 +49,7 @@ def get_config():
             'run_name': get_env_var('RUN_NAME'),
             'temp_dir': get_env_var('TEMP', optional=True, default='/tmp'),
             'namespace_file': os.path.join(get_env_var('NAMESPACE')),
-            'logging_level': logging.INFO,
+            'logging_level': DEBUG_LEVEL.get(get_env_var('LOGGING', optional=True, default='INFO'), logging.INFO),
             'num_of_processor': int(get_env_var('NUM_PROC', optional=True, default='2')),
             'kb_to_fbid_mapping': get_env_var('KB_FBID_MAPPING', optional=True),
             'enable_cmu_gid_patch': get_env_var('ENABLE_CMU_GID_PATCH', optional=True),
